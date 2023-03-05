3d87fc8359a1ec035f5552547ced44e0d58580a0
Sichu Jiang <jiangsichu@gmail.com>
2023-03-05 13:24:02 +0000

    Set up scrapy project

---
+++ spiders/houses_spider.py
@@ -0,0 +1,38 @@
+    
+import os
+import scrapy
+import smtplib
+from email.mime.text import MIMEText
+
+
+
+def send_mail(subject, body):
+    msg = MIMEText(body or "")
+    msg['Subject'] = subject
+    msg['From'] = MAIL_FROM
+    msg['To'] = ', '.join(MAIL_TO)
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.login(MAIL_FROM, MAIL_PWD)
+    smtp_server.sendmail(MAIL_FROM, MAIL_TO, msg.as_string())
+    smtp_server.quit()
+
+class HousesSpider(scrapy.Spider):
+    name = "houses"
+    start_urls = ["https://www.bellway.co.uk/new-homes/kent/alkerden-heights"]
+        
+    def parse(self, response):
+        for house in response.css('div#house-styles article'):
+            title = house.css('span.result-title::text').get()
+            price = house.css('span.result-pricing strong::text').get()
+            self.logger.debug('House type: %s, price: %s', title, price)
+            
+            if not os.path.exists(MARKER_FILE_NAME) and title == SEEKED_HOUSE:
+                self.logger.info('Found %s! Sending email.', title)
+                send_mail(subject=title, body=price)
+                f = open(MARKER_FILE_NAME, "x")
+                f.close()
+
+            yield {
+                'title': title,
+                'price': price,
+            }
